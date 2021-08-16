6a8683775d7e299f1e121ad28a239b959d1e67be
Pablo Asencio <8017043+PabloAsencio@users.noreply.github.com>
2021-08-16 10:06:25 +0200

    Refactor guessinggame.sh to include a function

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
 # File: guessinggame.sh
 
-number_of_files=$(ls | wc -l)
-echo "What is the number of files in the current directory?"
-echo "Take a guess!"
-read guess
-while [[ $guess -ne $number_of_files ]] 
-do
+function toohighortoolow {
 	if [[ $guess -gt $number_of_files ]]
 	then 
 		echo -n "You guessed too high."
 	else 
 		echo -n "You guessed too low."
 	fi
+
+}
+
+
+number_of_files=$(ls | wc -l)
+echo "What is the number of files in the current directory?"
+echo "Take a guess!"
+read guess
+while [[ $guess -ne $number_of_files ]] 
+do
+	toohighortoolow
 	echo "Try again!"
 	read guess
 done
